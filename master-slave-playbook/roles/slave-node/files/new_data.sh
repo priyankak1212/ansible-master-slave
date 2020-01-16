2a5eca623c37dacd888ae01bd4e7735642da9559
priyankak1212 <56244830+priyankak1212@users.noreply.github.com>
2020-01-16 15:32:19 +0530

    Update new_data.sh

--- master-slave-playbook/roles/slave-node/files/new_data.sh
+++ master-slave-playbook/roles/slave-node/files/new_data.sh
@@ -30,5 +30,5 @@ chmod 777 .ssh
 touch  .ssh/authorized_keys
 chmod 700 .ssh/authorized_keys
 cat >> /home/ansible/.ssh/authorized_keys <<EOF
-ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCeu9ZKD55XShgl2jciXiI0XKAqaETjsHtfryF3mRdAh3wHmGogMZPgXf8N/NjMXNFfhL4oQYj4hg0a83/BH7FbH3vumwVmzaG0/vAOeVszeSYeFSXYF5fuIdDTBYOykgU7lvUbTid3vJLbFT2jJ0S4vlRF1oxOqcW1L/UXJSAHfJ4VZ8qoOBGBvCQbI5hMI2Eg+mpauDeMMGfGBIZQIRGFRS2w+lP8jXKrlcpzN9JYwj3QOkCAR/mUn4IP+J/SIn80yHG9dTemxCvcPpczGViAp3Ae+g37XhYERK+/Un5vArZ7dQLZHr7ansLfrJ5jxPSCdWqAbrBB8XBQjmEwmLTD ansible@ip-172-31-83-146.ec2.internal
+
 EOF
